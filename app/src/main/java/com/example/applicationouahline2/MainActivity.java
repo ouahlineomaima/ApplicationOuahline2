5c87228ff8eaa9e48b6db0e89f2c5df620cafa2d
ouahlineomaima <ouahline.omaima@ine.inpt.ma>
2022-12-22 20:56:06 +0100

    Initializing the project

---
+++ app/src/main/java/com/example/applicationouahline2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.applicationouahline2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // comment
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
