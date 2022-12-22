282425adcb2f8fe5116179b35527341683dc5d83
ouahlineomaima <ouahline.omaima@ine.inpt.ma>
2022-12-22 21:55:17 +0100

    Question1

--- app/src/main/java/com/example/applicationouahline2/MainActivity.java
+++ app/src/main/java/com/example/applicationouahline2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.applicationouahline2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
     // comment1
@@ -10,6 +12,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        // RelativeLayout
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        relativeLayout.setLayoutParams(new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT));
+        //relativeLayout.setId(R.id.relative_layout);
+        // WebView
+        WebView webView = new WebView(this);
+        webView.setLayoutParams(new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT));
+        //webView.setId(R.id.web_view);
+        webView.loadUrl("https://www.example.com");
+        relativeLayout.addView(webView);
+
+
+
     }
 }
