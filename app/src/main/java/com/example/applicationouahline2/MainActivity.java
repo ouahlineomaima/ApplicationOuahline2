17c26f12321565339cf7e8ca55cfd52231f48803
ouahlineomaima <ouahline.omaima@ine.inpt.ma>
2022-12-29 15:51:17 +0100

    Question 2

--- app/src/main/java/com/example/applicationouahline2/MainActivity.java
+++ app/src/main/java/com/example/applicationouahline2/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
@@ -17,66 +19,39 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
-    EditText messageTexte;
-    String phoneNo;
-    String message;
-    Button bouttonEnvoi;
-    EditText numeroTelephone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        messageTexte = (EditText) findViewById(R.id.editText);
-        bouttonEnvoi = (Button) findViewById(R.id.button);
-        numeroTelephone = (EditText) findViewById(R.id.editText2);
-
-        bouttonEnvoi.setOnClickListener(new View.OnClickListener() {
+        Button boutonDemarrer = findViewById(R.id.sendEmail);
+        boutonDemarrer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                envoyerSMS();
+                envoyerEmail();
             }
         });
     }
 
-    protected void envoyerSMS(){
-        phoneNo = numeroTelephone.getText().toString();
-        message = messageTexte.getText().toString();
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)){
-                Log.i(null,"dkhalna1");
+    protected void envoyerEmail(){
+        Log.i("Send Email", "");
+        String [] TO = {""};
+        String [] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Sujet du mail");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Contenu du mail");
 
-            }
-            else{
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
-            }
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Envoyer mail... "));
+            finish();
+            Log.i("Envoi du mail terminé", "");
         }
-        try{
-            SmsManager gestionnaireSMS = SmsManager.getDefault();
-            gestionnaireSMS.sendTextMessage(phoneNo, null, message, null, null);
-            Toast.makeText(getApplicationContext(), "SMS envoyé", Toast.LENGTH_LONG).show();
-        }
-        catch(Exception e){
-            Toast.makeText(getApplicationContext(), "SMS non envoyé", Toast.LENGTH_LONG).show();
-            return;
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.i(null,"dkhalna2");
-                    SmsManager gestionnaireSMS = SmsManager.getDefault();
-                    gestionnaireSMS.sendTextMessage(phoneNo, null, message, null, null);
-                    Toast.makeText(getApplicationContext(), "SMS envoyé", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "SMS non envoyé", Toast.LENGTH_LONG).show();
-                    return;
-                }
-            }
+        catch (android.content.ActivityNotFoundException ex){
+            Toast.makeText(MainActivity.this, "Pas de client mail installé", Toast.LENGTH_SHORT).show();
         }
     }
 
