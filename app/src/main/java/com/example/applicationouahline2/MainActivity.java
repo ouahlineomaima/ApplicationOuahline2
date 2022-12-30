32b72cfb7b8c32821a4987a5e2188738ed3b5229
ouahlineomaima <ouahline.omaima@ine.inpt.ma>
2022-12-30 22:33:20 +0100

    Question1

--- app/src/main/java/com/example/applicationouahline2/MainActivity.java
+++ app/src/main/java/com/example/applicationouahline2/MainActivity.java
@@ -1,57 +1,47 @@
 package com.example.applicationouahline2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
 
-import android.Manifest;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-    private EditText phoneField;
-    private final static int REQUEST_PERMISSION_PHONE = 0;
+    Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        button = findViewById(R.id.button);
-        phoneField = findViewById(R.id.editText);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String phoneNumber = phoneField.getText().toString();
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:"+phoneNumber));
-
-                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
-                    requestPermissions(new String[] {Manifest.permission.CALL_PHONE}, REQUEST_PERMISSION_PHONE);
-                }
-                else{
-                    startActivity(callIntent);
-                }
-            }
-        });
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_PERMISSION_PHONE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "L'appel peut être lancée maintenant", Toast.LENGTH_SHORT).show();
-            }
-        }
+    public void sendNotification(View view){
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.rockstargames.com/gta-v"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        String CHANNEL_ID = "channel_1";
+
+        mBuilder.setContentIntent(pendingIntent);
+
+        mBuilder.setSmallIcon(R.drawable.ic_launcher_background);
+        mBuilder.setContentTitle("My notification");
+        mBuilder.setContentText("Hello World!");
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(001, mBuilder.build());
     }
+
 
 }
