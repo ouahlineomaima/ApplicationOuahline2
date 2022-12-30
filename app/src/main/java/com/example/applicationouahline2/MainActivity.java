80cd97b56b2fb34e7f32e25c185ca60bc5a2cdda
ouahlineomaima <ouahline.omaima@ine.inpt.ma>
2022-12-30 22:03:31 +0100

    Question4

--- app/src/main/java/com/example/applicationouahline2/MainActivity.java
+++ app/src/main/java/com/example/applicationouahline2/MainActivity.java
@@ -10,30 +10,48 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private EditText phoneField;
+    private final static int REQUEST_PERMISSION_PHONE = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        phoneField = findViewById(R.id.editText);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent callIntent = new Intent(Intent.ACTION_DIAL);
-                callIntent.setData(Uri.parse("tel:00212616449925"));
+                String phoneNumber = phoneField.getText().toString();
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:"+phoneNumber));
 
                 if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
-                    return;
+                    requestPermissions(new String[] {Manifest.permission.CALL_PHONE}, REQUEST_PERMISSION_PHONE);
                 }
-                startActivity(callIntent);
+                else{
+                    startActivity(callIntent);
+                }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION_PHONE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "L'appel peut être lancée maintenant", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
